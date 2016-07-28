e780b21e609d2ccd63aa508b74915c043b8a6578
joaomiguelrcf <joaomiguelrcf@gmail.com>
2016-07-28 17:25:52 +0100

    Coursera exercise
    Completed exercise

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,85 @@
-## Put comments here that give an overall description of what your
-## functions do
+## FUNCTION 1
 
-## Write a short comment describing this function
+## DESCRIPTION:
+
+# The makeCacheMatrix function, when called, returns a list of 4 functions: set, get, setInverse and createInverse.
+# The function also sets 2 variables in those functions' environment - x and inverse:
+#   "x" is the matrix whose inverse you wish to calculate 
+#   "inverse" is its inverse matrix
+# By default, both "x" and "inverse" are empty matrices
+
+# The functions in the list set and get the values for "x" and "inverse":
+#   get() and set() return and set, respectively, the value of "x"
+#   getInverse() and setInverse() return and set, respectively, the value of "inverse"
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## serves to store the matrix 'x' and its inverse in cache
+    inverse <- NULL
+    set <- function(y) {
+        #write matrix
+        x <<- y
+        inverse <<- NULL
+    }
+    get <- function() x
+        #return matrix
+    setInverse <- function(newInverse){
+        #cache new inverse
+        inverse <<- newInverse
+    } 
+    getInverse <- function() inverse
+        #return cached inverse
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
+## FUNCTION 2
 
-## Write a short comment describing this function
+## DESCRIPTION:
+
+# The following function calculates the inverse of the matrix "x" passed to the above function
+# However, it first checks to see if the inverse has already been calculated.
+# If so, it gets the inverse from the cache and skips the computation. Otherwise, it calculates the inverse
+# of the matrix and sets the value of the inverse in the cache via the setInverse() function.
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getInverse()
+    if(!is.null(inverse)) {
+        #returns current cached inverse if it exists
+        message("getting cached data")
+        return(inverse)
+    }
+        #if it doesn't exist, calculates and returns the inverse, provided the matrix is invertible
+    data <- x$get()
+    inverse <- solve(data)
+    x$setInverse(inverse)
+    return(inverse)
 }
+
+
+##TESTING
+
+#Matrices for testing:
+
+A = matrix(c( 7, 2, 1,
+              0, 3,-1,
+             -3, 4,-2),3,3,byrow = TRUE) #3x3 matrix
+
+B = diag(1,4,4) #identity matrix 4x4
+
+C = matrix(c(2, 3, 1, 5 ,
+             1, 0, 3, 1 ,
+             0, 2, -3, 2 ,
+             0, 2, 3, 1),4,4, byrow = TRUE) #4x4 matrix
+
+#inverse matrices for checking
+solve(A)
+solve(B)
+solve(C)
+
+#testing the functions
+x = makeCacheMatrix(C)
+cacheSolve(x)
+
